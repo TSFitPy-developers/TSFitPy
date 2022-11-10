2321350fce2885471f565b00e866172be1ccd01b
stormnick <fifthrom@gmail.com>
2022-11-10 12:27:12 +0100

    bug fix no metal load for only 1 spectra fit

--- scripts/TSFitPy.py
+++ scripts/TSFitPy.py
@@ -1013,8 +1013,8 @@ def run_TSFitPy():
                                                                                            unpack=True)
 
     if np.size(specname_fitlist) == 1:
-        specname_fitlist, rv_fitlist, teff_fitlist, logg_fitlist = np.array([specname_fitlist]), np.array(
-            [rv_fitlist]), np.array([teff_fitlist]), np.array([logg_fitlist])
+        specname_fitlist, rv_fitlist, teff_fitlist, logg_fitlist, met_fitlist = np.array([specname_fitlist]), np.array(
+                [rv_fitlist]), np.array([teff_fitlist]), np.array([logg_fitlist]), np.array([met_fitlist])
 
     if Spectra.fit_microturb == "Input":
         microturb_input = np.loadtxt(fitlist, dtype='str', usecols=5, unpack=True)
