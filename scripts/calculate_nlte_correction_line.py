a25033b3c5fa417221baaa460f7652f0871ac826
Korboal <fifthrom@gmail.com>
2023-03-21 12:25:46 +0100

    ok nelder-mead is the only option

--- scripts/calculate_nlte_correction_line.py
+++ scripts/calculate_nlte_correction_line.py
@@ -201,12 +201,10 @@ def generate_and_fit_atmosphere(specname, teff, logg, microturb, met, lmin, lmax
                                                         line_list_path, element, abundance, False)
     ew_lte = calculate_equivalent_width(wavelength_lte, norm_flux_lte, lmin - 3, lmax + 3) * 1000
     print(f"Fitting {specname} Teff={teff} logg={logg} [Fe/H]={met} microturb={microturb} line_center={line_center} ew_lte={ew_lte}")
-    result = minimize(get_nlte_ew, [abundance - 0.3],
+    result = minimize(get_nlte_ew, [abundance - 0.3, abundance + 0.3],
                       args=(teff, logg, microturb, met, lmin, lmax, ldelta, line_list_path, element, ew_lte),
-                      bounds=[(abundance - 3, abundance + 3)], method="L-BFGS-B",
-                      options={'maxiter': 100, 'disp': False})
-                      #bounds = [(abundance - 3, abundance + 3)], method = "Nelder-Mead",
-                      #options={'maxiter': 100, 'disp': False, 'fatol': 1e-9, 'xatol': 1e-6})  # 'eps': 1e-8
+                      bounds=[(abundance - 3, abundance + 3)], method="Nelder-Mead",
+                      options={'maxiter': 100, 'disp': False, 'fatol': 1e-9, 'xatol': 1e-6})  # 'eps': 1e-8
 
     nlte_correction = result.x[0]
     ew_nlte = np.sqrt(result.fun) + ew_lte
