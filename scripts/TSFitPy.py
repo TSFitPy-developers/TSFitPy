f9b580e9b0211c8f0dadea18c4b00ce6fcdba4d1
Korboal <fifthrom@gmail.com>
2023-05-17 15:52:28 +0200

    turned off upper limit

--- scripts/TSFitPy.py
+++ scripts/TSFitPy.py
@@ -994,7 +994,7 @@ class Spectra:
         """
         result = {}
         result_upper_limit = {}
-        find_upper_limit = True
+        find_upper_limit = False
         sigmas_upper_limit = 0.333
 
         if self.dask_workers > 1 and self.experimental_parallelisation:
