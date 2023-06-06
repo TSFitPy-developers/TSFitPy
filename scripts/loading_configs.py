0b49d5d0a2a5d24b9a352ed262be945334217595
Korboal <fifthrom@gmail.com>
2023-06-06 08:07:08 -0500

    working on the loading config

--- scripts/loading_configs.py
+++ scripts/loading_configs.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
-from scripts.solar_abundances import periodic_table
+from scripts.solar_abundances import periodic_table, solar_abundances
 
 class FitList:
     def __init__(self, input_file_path: str, first_row_name: bool):
         # read in the atmosphere grid to compute for the synthetic spectra
         # Read the file
+        # argument in pd.read_csv to read first column not as index but as a column
+        # https://stackoverflow.com/questions/36519086/pandas-read-csv-skips-first-column-names
         df = pd.read_csv(input_file_path, delim_whitespace=True)
 
         # Create a dictionary that maps non-standard names to standard ones
@@ -23,6 +25,7 @@ class FitList:
         abundances_xfe_given = []
         abundances_xh_given = []
         abundances_x_given = []
+        self.abundance_elements_given: list[str] = []
 
         for col in df.columns:
             # Replace the column name if it's in the dictionary, otherwise leave it unchanged
@@ -33,34 +36,50 @@ class FitList:
                 starting_element = testing_col[:-2].capitalize()
                 if ending_element == "fe" and starting_element in periodic_table:
                     # means X/Fe
-                    standard_name = f'{starting_element}fe'
+                    standard_name = f"{starting_element.lower()}"
                     abundances_xfe_given.append(standard_name)
+                    self.abundance_elements_given.append(standard_name.capitalize())
                 elif ending_element[-1] == 'h' and starting_element[:-1] in periodic_table:
                     # means X/H
-                    standard_name = f'{starting_element[:-1]}h'
+                    standard_name = f"{starting_element[:-1].lower()}"
                     abundances_xh_given.append(standard_name)
+                    self.abundance_elements_given.append(standard_name.capitalize())
                 elif np.size(testing_col) <= 2 and testing_col in periodic_table:
                     # means just elemental abundance perhaps
-                    standard_name = testing_col
+                    standard_name = f"{testing_col.lower()}"
                     abundances_x_given.append(standard_name)
-                elif:
-
+                    self.abundance_elements_given.append(standard_name.capitalize())
+                elif np.size(testing_col) <= 3 and testing_col[1:].capitalize() in periodic_table and testing_col[0].lower() == 'a':
+                    # means just elemental abundance perhaps because A(X) is given
+                    standard_name = f"{testing_col[1:].lower()}"
+                    abundances_x_given.append(standard_name)
+                    self.abundance_elements_given.append(standard_name.capitalize())
                 else:
                     # could not parse, not element?
                     standard_name = col
             df.rename(columns={col: standard_name}, inplace=True)
 
-        #xfe = xh - feh
+        # make all columns lower
+        df.columns = df.columns.str.lower()
+
+        for element in abundances_xfe_given:
+            # capitalise
+            df.rename(columns={element: element.capitalize()}, inplace=True)
 
         # convert xh to xfe
         for element in abundances_xh_given:
+            # xfe = xh - feh
             df[element] = df[element] - df['feh']
+            # convert to capital letter
+            df.rename(columns={element: element.capitalize()}, inplace=True)
 
         # convert A(X) to xfe
-        for element in abundances_x_given
-
-        # make all columns lower
-        df.columns = df.columns.str.lower()
+        for element in abundances_x_given:
+            # [X/H]_star = A(X)_star - A(X)_sun
+            # xfe = [X/H]_star - feh
+            df[element] = df[element] - solar_abundances[element.capitalize()] - df['feh']
+            # convert to capital letter
+            df.rename(columns={element: element.capitalize()}, inplace=True)
 
         if first_row_name:
             # replace first column name with 'name'
@@ -86,15 +105,20 @@ class FitList:
         feh_list = self.spectra_parameters_df['feh'].values
         vmic_list = self.spectra_parameters_df['vmic'].values
         vmac_list = self.spectra_parameters_df['vmac'].values
+        # get abundance elements and set them in input_abundance_dict dictionary
+        input_abundance_dict = {}
+        for element in self.abundance_elements_given:
+            input_abundance_dict[element] = self.spectra_parameters_df[element].values
 
-        stacked_parameters = np.stack((specname_list, rv_list, teff_list, feh_list, vmic_list, vmac_list), axis=1)
+
+        stacked_parameters = np.stack((specname_list, rv_list, teff_list, feh_list, vmic_list, vmac_list, input_abundance_dict), axis=1)
 
         return stacked_parameters
 
 
     @staticmethod
     def _strip_string(string_to_strip: str) -> str:
-        bad_characters = ["[", "]", "/", "\\"]
+        bad_characters = ["[", "]", "/", "\\", "(", ")", "{", "}"]
         for character_to_remove in bad_characters:
             string_to_strip = string_to_strip.replace(character_to_remove, '')
         return string_to_strip
@@ -104,6 +128,6 @@ class FitList:
         return self.spectra_parameters_df.to_string()
 
 if __name__ == '__main__':
-    fitlist = FitList('../input_files/fitlist', True)
+    fitlist = FitList('../input_files/fitlist_test', True)
     print(fitlist)
     print(fitlist.get_spectra_parameters_for_fit())
