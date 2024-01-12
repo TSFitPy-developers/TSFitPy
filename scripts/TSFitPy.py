95eaa0873fc72951914973c22bbf765f8d85ffe8
Korboal <fifthrom@gmail.com>
2024-01-12 15:31:59 +0100

    testing if m3dis module exists in the first place

--- scripts/TSFitPy.py
+++ scripts/TSFitPy.py
@@ -2411,6 +2411,10 @@ def run_tsfitpy(output_folder_title, config_location, spectra_location=None):
         if tsfitpy_configuration.cluster_type.lower() == 'slurm':
             # create a symlink to the m3dis executable
             m3dis_executable_path = os.path.join(tsfitpy_configuration.spectral_code_path, "m3dis", "")
+            # check if m3dis_executable_path exists
+            if not os.path.exists(m3dis_executable_path):
+                raise ValueError(f"m3dis executable path {m3dis_executable_path} does not exist!")
+
             m3dis_executable_path_symlink = "./m3dis/"
             if os.path.exists(m3dis_executable_path_symlink):
                 if os.path.islink(m3dis_executable_path_symlink):
