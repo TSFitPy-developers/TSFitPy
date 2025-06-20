abfd86843c582d396b7af0caa1202a29c98e0a6f
Korboal <fifthrom@gmail.com>
2025-06-20 11:48:58 +0200

    working on the nlte grid downloading

---
+++ utilities/download_nlte_grids.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import pandas as pd
+
+matplotlib.use("MacOSX")
+# plt.style.use("/Users/storm/PycharmProjects/bensby_3d_nlte/Bergemann2020.mplstyle")
+
+# Created by storm at 20.06.25
